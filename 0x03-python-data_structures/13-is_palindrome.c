a7949080f1d7ea085b86ef7d839b9ec5e08875e7
TachiZakariae <zakariaetachi@gmail.com>
2024-03-16 16:50:18 -0700

    Python - Data Structures: Lists, Tuples

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,11 +1,12 @@
 #include "lists.h"
 
 /**
- * palindrome - recursive palind or not
+ * palindrom - recursive palind or not
  * @head: head list
  * Return: 0 if it is not a palindrome
  * 1 if it is a palindrome
  */
+
 int is_palindrome(listint_t **head)
 {
 	if (head == NULL || *head == NULL)
@@ -18,6 +19,7 @@ int is_palindrome(listint_t **head)
  * @head: head list
  * @end: end list
  */
+
 int aux_palind(listint_t **head, listint_t *end)
 {
 	if (end == NULL)
