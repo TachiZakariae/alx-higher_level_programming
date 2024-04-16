bd12027c88580356016372b9fc38f281ed294974
TachiZakariae <zakariaetachi@gmail.com>
2024-04-16 10:20:27 -0700

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -40,7 +40,7 @@ void print_python_bytes(PyObject *p)
 	PyBytesObject *bytes = (PyBytesObject *)p;
 
 	printf("[.] bytes object info \n");
-	if (strcmp(p->ob_type->tp_name, "bytes")!= 0)
+	if (strcmp(p->ob_type->tp_name, "bytes") != 0)
 	{
 		printf("  [ERROR] Invalid Bytes Object\n");
 		return;
