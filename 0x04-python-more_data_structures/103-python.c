032209b5ffba9173aa55892f22183491e8b46ab8
TachiZakariae <zakariaetachi@gmail.com>
2024-04-16 10:19:21 -0700

    Python - More Data Structures: Set, Dictionary

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -46,4 +46,21 @@ void print_python_bytes(PyObject *p)
 		return;
 	}
 
+	printf(" size: %ld\n", ((PyVarObject *)p)->ob_size);
+	prinrf(" trying string: %s\n", bytes->ob_sval);
 
+	if (((PyVarObject *)p)->ob_size > 10)
+		size = 10;
+	else
+		size = ((PyVarObject *)p)->ob_size + 1;
+
+	printf(" first %d bytes: ", size);
+	for (i = 0; i < size; i++)
+	{
+		printf("%02hhx", bytes->ob_sval[i]);
+		if (i == (size - 1))
+			printf("\n");
+		else
+			printf(" ");
+	}
+}
